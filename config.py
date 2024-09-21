15bc314bba71acb5159b6a7a17041b479b122416
divyanshu230792 <divyanshusharma0005@gmail.com>
2024-09-22 00:31:45 +0530

    sss

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 FLASK_PORT_NO=8080
-CSV_FILENAME=r'data\employee_data.csv'
-COLUMN_DATA=r'artifacts\Emp_col_Data2.json'
-MODEL_FILENAME=r'artifacts\Emp_Data_model_pkl2.pickle'
+CSV_FILENAME=r'data/employee_data.csv'
+COLUMN_DATA=r'artifacts/Emp_col_Data2.json'
+MODEL_FILENAME=r'artifacts/Emp_Data_model_pkl2.pickle'
